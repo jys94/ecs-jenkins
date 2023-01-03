b526ce3d9224f5f02cf719e7c858a601df9b1679
jys94 <115811321+jys94@users.noreply.github.com>
2023-01-04 02:59:50 +0900

    Update configure-remote-state.sh

--- scripts/configure-remote-state.sh
+++ scripts/configure-remote-state.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-echo "Newer terraform versions have a new way of defining a backend. Copy paste the following code in a backend.tf file, modify the region/s3 bucket, and execute 'terraform init' to initialize the backend. You'll be asked to copy the data from the local backend to the s3 backend, which you can answer yes.
-"
-echo 'backend.tf
-==========
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-a3c731f"
-    key    = "terraform.tfstate"
-    region = "ap-northeast-2"
-  }
-}
-'
+AWS_REGION="ap-northeast-2"
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
+sed -i 's/#//g' backend.tf
+terraform init
+terraform apply -target aws_ecs_service.myapp-service -var MYAPP_SERVICE_ENABLE="1" -var MYAPP_VERSION=${MYAPP_VERSION}
