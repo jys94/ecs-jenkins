8848e0570ad89115e01c1eb1b26e8a0926e3aca1
jys94 <115811321+jys94@users.noreply.github.com>
2023-01-04 09:38:45 +0900

    Update configure-remote-state.sh

--- scripts/configure-remote-state.sh
+++ scripts/configure-remote-state.sh
@@ -5,4 +5,4 @@ S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 terraform init
-terraform apply -target aws_ecs_service.myapp-service -var MYAPP_SERVICE_ENABLE="1" -var MYAPP_VERSION=${MYAPP_VERSION}
+terraform apply -target aws_ecs_service.myapp-service -var MYAPP_SERVICE_ENABLE="1" -var MYAPP_VERSION=${MYAPP_VERSION} -auto-approve
